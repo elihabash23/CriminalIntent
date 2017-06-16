2670f4ede2f47fd2800047931f964b12a6d0ee1f
Elias Habash <eliashabash@aol.com>
2017-06-16 16:04:29 -0700

    Added activity_crime_pager.xml

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 /**
@@ -7,4 +8,11 @@ import android.support.v4.app.FragmentActivity;
  */
 
 public class CrimePagerActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_crime_pager);
+    }
 }
