0f092a7f196d6b6eb0f7c8a01c1594e53ba9ac3f
Elias Habash <eliashabash@aol.com>
2017-07-12 00:35:12 -0700

    Changed visibility of updateUI so it can be called from CrimeListActivity

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -211,7 +211,7 @@ public class CrimeListFragment extends Fragment {       // a controller of my MV
         activity.getSupportActionBar().setSubtitle(subtitle);
     }
 
-    private void updateUI() {
+    public void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
 
